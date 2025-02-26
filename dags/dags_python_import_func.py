9b3896e0d9f62345f83f165a8ef382dd35c66699
ghdtjrasl3 <ghdtjrals3@gmail.com>
2025-02-26 13:26:01 +0900

    공통 외부함수 추가

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,16 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="0 0 0 * *",
+    start_date=pendulum.datetime(2025,3,1,tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    task_get_stfp = PythonOperator(
+        task_id="task_get_stfp",
+        python_callable=get_sftp
+    )
