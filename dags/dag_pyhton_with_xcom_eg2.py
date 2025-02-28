43d889fa2502986339e06923582a5279c6f662d7
ghdtjrasl3 <ghdtjrals3@gmail.com>
2025-02-28 11:12:25 +0900

    python operator에서 xcom 사용 업로드

---
+++ dags/dag_pyhton_with_xcom_eg2.py
@@ -0,0 +1,34 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+with DAG(
+    dag_id="dag_python_with_xcom_eg2",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    @task(task_id='python_xcom_push_task1')
+    def xcom_push1(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key="result1", value="value_1")
+        ti.xcom_push(key="result2", value=[1,2,3])
+
+    @task(task_id='python_xcom_push_task2')
+    def xcom_push2(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key="result1", value="value_2")
+        ti.xcom_push(key="result2", value=[1,2,3,4])
+
+    # ti 객체로부터 xcom_pull 메소드를 사용하여 key 값을 통해 가져올 수 있음
+    # 그러나 같은 key 값의 ti 객체가 있다면 제일 마지막에 들어간 값을 기준으로 가져옴
+    # 따라서 task_ids로 task_id를 명시하여 원하는 ti 객체의 xcom 값을 가져올 수 있음
+    @task(task_id='python_xcom_pull_task')
+    def xcom_push2(**kwargs):
+        ti = kwargs['ti']
+        value1 = ti.xcom_pull(key="result1")
+        value2 = ti.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
+        print(value1)
+        print(value2)
+
+    xcom_push1() >> xcom_push2() >> xcom_pull()
--- dags/dag_python_with_xcom_eg1.py
+++ dags/dag_python_with_xcom_eg1.py
@@ -3,30 +3,30 @@ import pendulum
 from airflow import DAG
 from airflow.decorators import task
 with DAG(
-    dag_id="dag_python_with_xcom_eg1.py",
+    dag_id="dag_python_with_xcom_eg1",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2025, 2, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    @task(task_id='python_xcom_push_task1')
-    def xcom_push1(**kwargs):
-        ti = kwargs['ti']
-        ti.xcom_push(key="result1", value="value_1")
-        ti.xcom_push(key="result2", value=[1,2,3])
+    @task(task_id='python_xcom_push_by_return')
+    def xcom_push_result(**kwargs):
+        return 'Success'
 
-    @task(task_id='python_xcom_push_task2')
-    def xcom_push2(**kwargs):
-        ti = kwargs['ti']
-        ti.xcom_push(key="result1", value="value_2")
-        ti.xcom_push(key="result2", value=[1,2,3,4])
+    @task(task_id='python_xcom_pull_1')
+    def xcom_pull_1(**kwargs):
+        t1 = kwargs['ti']
+        # xcom_pull 파라미터로 task_ids만 넣는다면 디폴트로 해당 task의 return 값을 가져옴 'Success'
+        value1 = t1.xcom_pull(task_ids='python_xcom_push_by_return')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값:' + value1)
 
-    @task(task_id='python_xcom_pull_task')
-    def xcom_push2(**kwargs):
-        ti = kwargs['ti']
-        value1 = ti.xcom_pull(key="result1")
-        value2 = ti.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
-        print(value1)
-        print(value2)
+    # status는 해당 DAG의 flow를 이해해야함
+    # 현재 python_xcom_push_by_return = xcom_push_result()를 통해
+    # Success 값이 python_xcom_push_by_return 테스크의 return xcom 값으로 들어가 있음
+    # 그 후 xcom_pull_2(python_xcom_push_by_return)는 return xcom 값을 status 인자로 호출함
+    @task(task_id='python_xcom_pull_2')
+    def xcom_pull_2(status, **kwargs):
+        print('함수 입력값으로 받은 값:' + status)
 
-    xcom_push1() >> xcom_push2() >> xcom_pull()
-
+    python_xcom_push_by_return = xcom_push_result()
+    xcom_pull_2(python_xcom_push_by_return)
+    python_xcom_push_by_return >> xcom_pull_1()
