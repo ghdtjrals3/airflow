86cb3c2e75d7333ba777d8d17931ab032459b25f
ghdtjrasl3 <ghdtjrals3@gmail.com>
2025-03-05 16:51:45 +0900

    airflow simplehttp 업로드

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+import pendulum
+
+with DAG(
+    dag_id='dags_simple_http_operator',
+    start_date=pendulum.datetime(2025,2,1, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None
+) as dag:
+    '''감염병(코로나) 확진자 발생동향'''
+    covid19_info = SimpleHttpOperator(
+        task_id='covid19_info',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/xml/TbCorona19CountStatus/1/5/',
+        method='GET',
+        header={'Content-Type': 'application/json',
+                'charset': 'utf-8',
+                'Accept': '*/*'
+                }
+
+    )
+
+    @task(task_id='pyhton_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='covid19_info')
+        import json
+        from pprint import pprint
+
+        pprint(json.loads(rslt))
+        
+    covid19_info >> python_2()
