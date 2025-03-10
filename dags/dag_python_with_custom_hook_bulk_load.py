2008e72dd328fc7a18d52ce500a52cb09e054f15
ghdtjrasl3 <ghdtjrals3@gmail.com>
2025-03-10 20:22:55 +0900

    custom, postgres hook dag upload

--- dags/dag_python_with_custom_hook_bulk_load.py
+++ dags/dag_python_with_custom_hook_bulk_load.py
@@ -1,22 +1,22 @@
 from airflow import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from hooks.custom_postgres_hook import CustomPostgresHook
 
 with DAG(
-    dag_id='dag_python_with_postfres_hook_bulk_load',
+    dag_id='dag_python_with_custom_hook_bulk_load',
     schedule='0 7 * * *',
     start_date=pendulum.datetime(2025,2,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
-        postgres_hook = PostgresHook(postgres_conn_id)
-        postgres_hook.bulk_load(tbl_nm, file_nm)
+        custom_postgres_hook = CustomPostgresHook(postgres_conn_id=postgres_conn_id)
+        custom_postgres_hook.bulk_load(table_name=tbl_nm, file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
-                  'tbl_nm':'TbCorona19CountStatus_bulk1',
+                  'tbl_nm':'TbCorona19CountStatus_bulk2',
                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
     )
