6b77c4abecdf29e072d57e20b8b2536cc550d8de
ghdtjrasl3 <ghdtjrals3@gmail.com>
2025-03-29 18:05:31 +0900

    실습 with gpt 업로드

---
+++ plugins/config/get_kakao_token.py
@@ -0,0 +1,16 @@
+import requests
+
+clinet_id = '-'
+redirect_uri = 'https://example.com/oauth'
+authorize_code = '-'
+
+token_url = 'https://kauth.kakao.com/oauth/token'
+data = {
+    'grant_type': 'authorization_code',
+    'client_id': clinet_id,
+    'redirect_uri': redirect_uri,
+    'code': authorize_code
+}
+response = requests.post(token_url, data=data)
+tokens = response.json()
+print(tokens)
